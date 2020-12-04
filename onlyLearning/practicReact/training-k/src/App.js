ff2ca3654d38a1ff804d963470ab25e5097d124b
Tainaka2003 <di.mlik@yandex.ru>
2020-12-04 19:12:15 +0300

    trainingK

---
+++ onlyLearning/practicReact/training-k/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+    return (
+        <div>
+            <div className="App">
+                <ul>
+                    <li>Не член</li>
+                    <li>Не член</li>
+                    <li>Не член</li>
+                    <li>Не член</li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default App;
