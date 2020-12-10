e150d7aab76a000e1a215e3ef3d02abd8250339b
Tainaka2003 <di.mlik@yandex.ru>
2020-12-10 14:01:57 +0300

    aaa

--- onlyLearning/practicReact/training-k/src/App.js
+++ onlyLearning/practicReact/training-k/src/App.js
@@ -1,17 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
     return (
         <div>
+            <Header/>
+            <NewHeader />
+        </div>
+    );
+}
+
+const Header = () => {
+    return (
+        <div>
             <div className="App">
                 <ul>
-                    <li>Не член</li>
-                    <li>Не член</li>
-                    <li>Не член</li>
-                    <li>Не член</li>
+                    <li>Член</li>
+                    <li>Член</li>
+                    <li>Член</li>
+                    <li>Член</li>
                 </ul>
             </div>
+        </div>
+    );
+}
+
+
+const NewHeader = () => {
+    return (
+        <div className="App">
+            <ul>
+                <li>Не член</li>
+                <li>Не член</li>
+                <li>Не член</li>
+                <li>Не член</li>
+            </ul>
         </div>
     );
 }
