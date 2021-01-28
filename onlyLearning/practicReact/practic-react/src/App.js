1cc1b9ef6c3ea486de903c32b366d3cefde3bc7b
Tainaka2003 <di.mlik@yandex.ru>
2021-01-28 18:14:15 +0300

    game+news

--- onlyLearning/practicReact/practic-react/src/App.js
+++ onlyLearning/practicReact/practic-react/src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import Form from './components/Form/index'
 
 class App extends Component{
@@ -8,14 +8,17 @@ class App extends Component{
             {
                 count: 0,
                 name: 'Ford',
-                year: '2018'},
+                year: '2018'
+            },
             {
                 count: 1,
                 name: 'Audi',
-                year: '2016'}
+                year: '2016'
+            }
         ],
 
         title: 'Привет, я с Донбаса, но продам тебе машины',
+        showCars: false,
 
     }
 
@@ -25,34 +28,64 @@ class App extends Component{
         })
     }
 
+    toggleCarsHandler = () => {
+        this.setState({
+            showCars: !this.state.showCars,
+        })
+    }
+
+    changeTitleHandler = pageTitle => {
+        this.setState({pageTitle})
+    }
+
     render() {
 
-        const cars = this.state.cars
+        const cars = this.state.cars //???
 
         return (
             <div>
                 <h3
-                    style={{textAlign: 'center'}}>
+                    style={{ textAlign: 'center' }}>
                     {this.state.title}
                 </h3>
+
                 <button
-                    onClick={this.changeTitleHandler.bind(this, 'Не продам я тебе ничего, чертила!')}
-                    style={{display: 'flex', justifyContent: 'center', margin: '0 auto'}}>
+                    onClick={this.toggleCarsHandler}
+                    style={{ display: 'flex', justifyContent: 'center', margin: '0 auto' }}>
                     Ладно
                 </button>
-                <Form
-                    name={cars[0].name}
-                    year={cars[0].year}
-                    onChange={this.changeTitleHandler.bind(this, 'Захотел ' + cars[0].name + ' ' + cars[0].year + ' года выпуска? Не получишь!')}
-                />
-                <Form
-                    name={cars[1].name}
-                    year={cars[1].year}
-                    onChange={() => { this.changeTitleHandler('Захотел ' + cars[1].name + ' ' + cars[1].year + ' года выпуска? Не получишь!')}}
-                />
-            </div>
-        )
-    }
-}
 
-export default App;
+                { this.state.showCars
+                    ? this.state.cars.map((car, index) => {
+                    return(
+                        <Form
+                            key={index}
+                            name={car.name}
+                            year={car.year}
+                            onChange={() => this.changeTitleHandler(car.name)} />
+                    )
+                })
+                    : null
+                }
+
+                    {/*<Form*/}
+                    {/*    name={cars[0].name}*/}
+                    {/*    year={cars[0].year}*/}
+                    {/*    onChange=*/}
+                    {/*        {this.changeTitleHandler.bind(this, 'Захотел ' + cars[0].name + ' ' + cars[0].year + ' года выпуска? Не получишь!')}*/}
+                    {/*/>*/}
+                    {/*<Form*/}
+                    {/*    name={cars[1].name}*/}
+                    {/*    year={cars[1].year}*/}
+                    {/*    onChange={() =>*/}
+                    {/*    { this.changeTitleHandler('Захотел ' + cars[1].name + ' ' + cars[1].year + ' года выпуска? Не получишь!')}}*/}
+                    {/*/>*/}
+
+
+
+                    </div>
+                    )
+                }
+                    }
+
+                    export default App;
--- onlyLearning/practicReact/react-training/src/components/Content/index.js
+++ onlyLearning/practicReact/react-training/src/components/Content/index.js
@@ -6,75 +6,76 @@ import NewsItem from "./NewsItem";
 class Content extends Component {
 
     state = {
-        count: 0,
-        game: [
-            {
-                name: 'Моя музыкальная одержимость',
-                year: '2020',
-                type: 'Школа, Повседневность',
-                description: 'Максим отправляется в музыкальную школу с целью осваивать новые умения игры. Казалось бы всё нормально, но для самой школы наступило непростое время, и она не готова обучать новых музыкантов...'
-            },
+        games: [
             {
                 name: 'Бесконечный Ягуар',
                 year: '2021',
                 type: 'Комедия, Трэш',
                 description: 'С 11 по 17 января в жизни русских визуальных новелл состоялся очередной эксперимент. У подопытных было только 2 пути - сделать новеллу за неделю и выжить или рассориться друг с другом и остаться в холоде навсегда...'
+            },
+            {
+                name: 'Моя музыкальная одержимость',
+                year: '2020',
+                type: 'Школа, Повседневность',
+                description: 'Максим отправляется в музыкальную школу с целью осваивать новые умения игры. Казалось бы всё нормально, но для самой школы наступило непростое время, и она не готова обучать новых музыкантов...'
             }
+
         ],
         news: [
             {
-                date: '1 июля 2020 года',
-                message: 'В этот день мы начали работать надо новеллой "Моя музыкальная одержимость". Формально, команды тогда не существовало, и все идеи держались на человеке с ником Tainaka, но без помощи друзей задумка новеллы так и осталась бы задумкой. В разное время разработке помогали ClassniChuvak, lonelypony, Иван Котовский, Time Project, Tekusa и др.'
-            },
-            {
-                date: '1 сентября 2020',
-                message: 'Вышла новелла "Моя музыкальная одержимость", которая рассказывала про деревенскую школу из недалёкого прошлого в не самой бедной стране из региона СНГ. Релиз был воспринят тихо - возможно ожидалось скорое продолжение.'
-            },
-            {
-                date: '19 декабря 2020',
-                message: 'Формальное создание "Лучших друзей". В состав вошли Tainaka, Kurosawa, ClassniChuvak, Rantom и Иван Котовский. В последствии по приглашению первого в составе появятся "Не могу придумать ник", Daniel_27 и trufata.'
+                date: '26 января 2021',
+                message: 'Состав команды сократился. Остались Tainaka, "Не могу придумать ник" и ClassniChuvak'
             },
             {
                 date: '16 января 2021',
                 message: 'Лучшие друзья приняли участие в локальном конкурсе на самую лучшую новеллу на скорость. В качестве конкурсной новеллы была представлена треш-новелла о том, как непросто делать подобного рода вещи под названием "Бесконечный Ягуар". Работа получила смешанные оценки.'
             },
             {
-                date: '26 января 2021',
-                message: 'Состав команды сократился. Остались Tainaka, "Не могу придумать ник" и ClassniChuvak'
-            }
-        ]
+                date: '19 декабря 2020',
+                message: 'Формальное создание "Лучших друзей". В состав вошли Tainaka, Kurosawa, ClassniChuvak, Rantom и Иван Котовский. В последствии по приглашению первого в составе появятся "Не могу придумать ник", Daniel_27 и trufata.'
+            },
+            {
+                date: '1 сентября 2020',
+                message: 'Вышла новелла "Моя музыкальная одержимость", которая рассказывала про деревенскую школу из недалёкого прошлого в не самой бедной стране из региона СНГ. Релиз был воспринят тихо - возможно ожидалось скорое продолжение.'
+            },
+            {
+                date: '1 июля 2020 года',
+                message: 'В этот день мы начали работать надо новеллой "Моя музыкальная одержимость". Формально, команды тогда не существовало, и все идеи держались на человеке с ником Tainaka, но без помощи друзей задумка новеллы так и осталась бы задумкой. В разное время разработке помогали ClassniChuvak, lonelypony, Иван Котовский, Time Project, Rantom, Tekusa и др.'
+            },
+        ],
+        showGames: true,
+        showNews: false,
+        showTeam: false,
+        showHelp: false
     }
 
     render() {
-        const game = this.state.game
-        const news = this.state.news
         return (
             <div className={"content"}>
-                <ContentItem
-                    name={game[1].name}
-                    year={game[1].year}
-                    type={game[1].type}
-                    description={game[1].description}/>
-                <ContentItem
-                    name={game[0].name}
-                    year={game[0].year}
-                    type={game[0].type}
-                    description={game[0].description}/>
-                <NewsItem
-                    date={news[4].date}
-                    message={news[4].message}/>
-                <NewsItem
-                    date={news[3].date}
-                    message={news[3].message}/>
-                <NewsItem
-                    date={news[2].date}
-                    message={news[2].message}/>
-                <NewsItem
-                    date={news[1].date}
-                    message={news[1].message}/>
-                <NewsItem
-                    date={news[0].date}
-                    message={news[0].message}/>
+                { this.state.showGames
+                    ? this.state.games.map((game, index) => {
+                    return(
+                        <ContentItem
+                            key={index}
+                            name={game.name}
+                            year={game.year}
+                            type={game.type}
+                            description={game.description} />
+                    )
+                })
+                    : null
+                }
+                { this.state.showNews
+                    ? this.state.news.map((caption, index) => {
+                    return(
+                        <NewsItem
+                            key={index}
+                            date={caption.date}
+                            message={caption.message} />
+                    )
+                })
+                    : null
+                }
             </div>
         )
     }
