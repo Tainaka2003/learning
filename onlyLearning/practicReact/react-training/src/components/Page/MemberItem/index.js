d8273610b8a0d671eab116b809130cd581daaaa1
Tainaka2003 <di.mlik@yandex.ru>
2021-06-02 18:45:22 +0300

    vue tz day 2

--- onlyLearning/practicReact/react-training/src/components/Page/MemberItem/index.js
+++ onlyLearning/practicReact/react-training/src/components/Page/MemberItem/index.js
@@ -4,7 +4,6 @@ import './style.css'
 const MemberItem = (props) => {
     return (
         <div className={"memberItem"}>
-            <div className={"memberImg"}></div>
             <p className={"paragraph"}>{props.member}</p>
             <p className={"paragraph"}>{props.role}</p>
         </div>
